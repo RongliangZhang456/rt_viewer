96a7f921246f1d06690bc0fb25e7a763c020a62b
HJJ-D <1078755455@qq.com>
2025-03-23 21:26:39 +0100

    interval.h

---
+++ src/rt_interval.h
@@ -0,0 +1,46 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+
+namespace rt
+{
+    class Interval {
+    public:
+        double min, max;
+
+        Interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+
+        Interval(double min, double max) : min(min), max(max) {}
+
+        double size() const {
+            return max - min;
+        }
+
+        bool contains(double x) const {
+            return min <= x && x <= max;
+        }
+
+        bool surrounds(double x) const {
+            return min < x && x < max;
+        }
+
+        double clamp(double x) const {
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
+        }
+
+        interval expand(double delta) const {
+            auto padding = delta / 2;
+            return interval(min - padding, max + padding);
+        }
+
+        static const Interval empty, universe;
+    };
+
+    const Interval Interval::empty = Interval(+infinity, -infinity);
+    const Interval Interval::universe = Interval(-infinity, +infinity);
+
+}
+
+#endif
